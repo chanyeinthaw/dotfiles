f0094780fcb65ee069afbc010c170ead79b73311
Chan Nyein Thaw <me@chanyeinthaw.com>
2021-01-30 16:40:53 +0630

    change lock and show temp

---
+++ scripts/lock.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+i3lock \
+--nofork \
+--image=/home/$USER/.config/i3/background.png \
+--clock \
+--indicator \
+--pass-media-keys \
+--pass-screen-keys \
+--pass-power-keys \
+--insidevercolor=00000000 \
+--insidewrongcolor=F4433650 \
+--insidecolor=00000000 \
+--ringcolor=90A4AEff \
+--ringvercolor=263238FF \
+--ringwrongcolor=263238FF \
+--line-uses-ring \
+--keyhlcolor=263238FF \
+--bshlcolor=FF5722FF \
+--verifcolor=ffffffff \
+--wrongcolor=ffffffff \
+--layoutcolor=ffffffff \
+--timecolor=ffffffff \
+--datecolor=ffffffff \
+--greetercolor=ffffffff \
+--greetertext=Pinch \
+--veriftext=... \
+--wrongtext=Nope \
+--radius=100
