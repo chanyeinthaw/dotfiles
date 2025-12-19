7fc605d00d527b356767ca2465fd581505e3a68d
Chan Nyein Thaw <chanyeinthaw@gmail.com>
2025-12-19 16:28:20 +0900

    Fix popuptmux.sh to open current pane path shell

--- dot-scripts/popuptmux.sh
+++ dot-scripts/popuptmux.sh
@@ -4,7 +4,8 @@ session="_popup_$(tmux display -p '#S')"
 
 if ! tmux has -t "$session" 2> /dev/null; then
   parent_session="$(tmux display -p '#{session_id}')"
-  session_id="$(tmux new-session -dP -s "$session" -F '#{session_id}' -e TMUX_PARENT_SESSION="$parent_session")"
+  parent_path="$(tmux display -p '#{pane_current_path}')"
+  session_id="$(tmux new-session -dP -s "$session" -F '#{session_id}' -c "$parent_path" -e TMUX_PARENT_SESSION="$parent_session")"
   tmux set-option -s -t "$session_id" key-table popup
   tmux set-option -s -t "$session_id" status off
   tmux set-option -s -t "$session_id" prefix None
